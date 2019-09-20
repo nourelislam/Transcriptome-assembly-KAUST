0b87ae884d7708d46ae945a0c77e7303c6eaee98
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2019-09-20 08:30:21 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -4,6 +4,6 @@ cat Oryza_sativa.IRGSP-1.0.dna.chromosome.1.fa Oryza_sativa.IRGSP-1.0.dna.chromo
 
 #hisat2 -x hisat2_idx/Oryza_sativa -q -1 M_19_0207_33-C-3-1_D701-D501_L001_R1_001.fastq M_19_0207_33-C-3-1_D701-D501_L001_R2_001.fastq -S M_19_0207_33-C-3-1_novel.sam -t --summary-file M_19_0207_33-C-3-1_with_novel.summary.txt --novel-splicesite-infile M_19_0207_33-C-3-1.tsv -p 4
 
-for R1 in *R1*; do R2=${R1//R1_001.fastq/R2_001.fastq}; sample=${R1%_D701-D501_L001_R1_001.fastq}; hisat2 -x hisat2_idx/Oryza_sativa -q -1 $R1 -2 $R2 -S $sample.sam -t --summary-file $sample.summary.txt --dta --novel-splicesite-outfile $sample.tsv -p 4; done
+for R1 in *R1*; do R2=${R1//R1_001.fastq/R2_001.fastq}; sample=${R1%-D501_L001_R1_001.fastq}; hisat2 -x hisat2_idx/Oryza_sativa -q -1 $R1 -2 $R2 -S $sample.sam -t --summary-file $sample.summary.txt --dta --novel-splicesite-outfile $sample.tsv -p 5; done
 # --dta for long anchor alignment 
 #--novel-splicesite-outfile to use it on the seceond run of alignment as an input of splice sites

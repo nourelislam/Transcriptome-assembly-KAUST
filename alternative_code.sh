84235d807f0eeff6e3f06d3a21ca958c7943d7ab
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-07-16 13:14:22 +0200

    Update alternative_code.sh

--- alternative_code.sh
+++ alternative_code.sh
@@ -1,2 +1,5 @@
 ## Assembly the merged bam files ##
 for file in *merged.sorted.bam; do sample=${file%-merged.sorted.bam}; stringtie -p 5 $file -o $sample.gtf -l $sample;done
+
+## Guided assembly ##
+for file in *merged.sorted.bam; do sample=${file%-merged.sorted.bam}; stringtie -G ../gtf/Oryza_sativa.IRGSP-1.0.44.gtf -p 5 $file -o $sample.gtf -l $sample;done

95777e481944b34d510a4937aa363125a22fbc33
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-09-08 23:13:28 +0200

    Update separate_bam.sh

--- separate_bam.sh
+++ separate_bam.sh
@@ -1,3 +1,4 @@
 ### merging all bam files ##
 stringtie --merge -p 8 *gtf -o merged.gtf
 ###then stringtie assembly one more time using merged as the reference file ##
+for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 -G GTF/merged.gtf $file -o $sample.guided.gtf -l $sample;done

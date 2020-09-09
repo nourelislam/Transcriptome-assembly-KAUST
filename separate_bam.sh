cdc064447d95d11f1468f909f33da67ccd28fdcd
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-09-09 11:06:06 +0200

    Update separate_bam.sh

--- separate_bam.sh
+++ separate_bam.sh
@@ -2,3 +2,7 @@
 stringtie --merge -p 8 *gtf -o merged.gtf
 ###then stringtie assembly one more time using merged as the reference file ##
 for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 -G GTF/merged.gtf $file -o $sample.guided.gtf -l $sample;done
+
+
+#####Guided assembly ###
+for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 -G ../../Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf $file -o ref_guided/$sample.ref_guided.gtf -l $sample;done

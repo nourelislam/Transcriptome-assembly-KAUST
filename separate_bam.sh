1fd87aecd9999bf3dd82f763b39ad74a4146fa0e
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-09-11 09:51:23 +0200

    Update separate_bam.sh

--- separate_bam.sh
+++ separate_bam.sh
@@ -7,3 +7,5 @@ for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 -G GTF/merged.gt
 
 #####Guided assembly ###
 for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 -G ../../Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf $file -o ref_guided/$sample.ref_guided.gtf -l $sample;done
+### featurecount ##
+featureCounts -T4 -a ../merged.ref_guided.gtf -g gene_id -o gene_counts.txt ~/Documents/DS/fastq/sorted/*.bam -G ~/Documents/DS/Whole_genome/whole_genome.fa 

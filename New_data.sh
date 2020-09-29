a6cd423dbf1ae5664f4bc30cccb90502f75e97cf
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-09-29 20:22:49 +0200

    Update New_data.sh

--- New_data.sh
+++ New_data.sh
@@ -2,3 +2,5 @@
 for R1 in *R1*; do R2=${R1//R1_001.fastq/R2_001.fastq}; sample=${R1%_R1_001.fastq}; hisat2 -x ../../Whole_genome/whole -q -1 $R1 -2 $R2 -S $sample.sam -t --dta --novel-splicesite-outfile $sample.tsv -p 8; rm $sample.sam; done
 #### 2nd iteration ##
 for R1 in *R1*; do R2=${R1//R1_001.fastq/R2_001.fastq}; sample=${R1%_R1_001.fastq}; hisat2 -x ../../Whole_genome/whole -q -1 $R1 -2 $R2 -S $sample.sam -t  --novel-splicesite-infile $sample.tsv -p 8; done
+### denovo assembly ###
+for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 $file -o $sample.gtf -l $sample;done

e186d412df10196b65b5d76445ab90b535a5a5a5
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-09-29 15:05:49 +0200

    Update New_data.sh

--- New_data.sh
+++ New_data.sh
@@ -1,1 +1,2 @@
-
+## alignment ##
+for R1 in *R1*; do R2=${R1//R1_001.fastq/R2_001.fastq}; sample=${R1%_R1_001.fastq}; hisat2 -x ../../Whole_genome/whole -q -1 $R1 -2 $R2 -S $sample.sam -t --dta --novel-splicesite-outfile $sample.tsv -p 8; rm $sample.sam; done

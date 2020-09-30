be4331268d741eca4de552f6b3999d7f821f2224
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-09-30 14:19:48 +0200

    Update New_data.sh

--- New_data.sh
+++ New_data.sh
@@ -6,3 +6,6 @@ for R1 in *R1*; do R2=${R1//R1_001.fastq/R2_001.fastq}; sample=${R1%_R1_001.fast
 for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 $file -o $sample.gtf -l $sample;done
 ### Assembly ##
 stringtie --merge -p 8 M-19-4801_WT-S-6-3_D706-D505_L001.gtf M-19-4801_WT-S-6-3_D706-D505_L002.gtf M-19-4802_WT-S-6-4_D706-D506_L001.gtf M-19-4802_WT-S-6-4_D706-D506_L002.gtf -o WT-S-merged.gtf -l WT-S
+
+### gff compare ##
+gffcompare *merged.gtf -r ~/Documents/DS/Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf -R -V

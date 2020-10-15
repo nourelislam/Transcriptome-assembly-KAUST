99ba61688ec6241aafcd5cd3083999dc204a2f86
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-10-15 09:38:23 +0200

    Update separate_bam.sh

--- separate_bam.sh
+++ separate_bam.sh
@@ -1,15 +1,9 @@
 for file in *.sam;do sample=${file%.sam}; samtools view -@ 7 -hbo $sample.bam $sample.sam; rm $sample.sam ; samtools sort -@ 7 $sample.bam -o $sample.sorted.bam; rm $sample.bam ;done
-### merging all bam files ##
-stringtie --merge -p 8 *gtf -o merged.gtf
-###then stringtie assembly one more time using merged as the reference file ##
-for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 -G GTF/merged.gtf $file -o $sample.guided.gtf -l $sample;done
+### merging all gtf files with reference gtf ##
+stringtie --merge -p10 -G ~/Documents/DS/Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf *gtf -o merged.gtf
 
 
-#####Guided assembly ###
-for file in *bam; do sample=${file%.sorted.bam}; stringtie -p 8 -G ../../Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf $file -o ref_guided/$sample.ref_guided.gtf -l $sample;done
 ### featurecount ##
-featureCounts -T4 -a ../merged.ref_guided.gtf -g gene_id -o gene_counts.txt ~/Documents/DS/fastq/sorted/*.bam -G ~/Documents/DS/Whole_genome/whole_genome.fa 
-featureCounts -T4 -a ../merged.ref_guided.gtf -g transcript_id -o transcript_counts.txt ~/Documents/DS/fastq/sorted/*.bam -G ~/Documents/DS/Whole_genome/whole_genome.fa
 
 cat gene_counts.txt | cut -f 1,7-30 > simple_gene_counts.txt
 cat transcript_counts.txt | cut -f 1,7-30 > simple_transcript_counts.txt

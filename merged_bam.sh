a38db8a3553b05387ce1eb7eff44b8b723b51d7d
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-10-15 09:09:40 +0200

    Update merged_bam.sh

--- merged_bam.sh
+++ merged_bam.sh
@@ -3,3 +3,6 @@ stringtie --merge -p12 -G ~/Documents/DS/Whole_genome/Oryza_sativa.IRGSP-1.0.44.
 
 ## compare #
 gffcompare merged.gtf -r ~/Documents/DS/Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf -V
+
+# feature count ##
+featureCounts -T10 -a gffcmp.annotated.gtf -g transcript_id -o transcript_counts.txt ../*.bam  -G ~/Documents/DS/Whole_genome/whole_genome.fa

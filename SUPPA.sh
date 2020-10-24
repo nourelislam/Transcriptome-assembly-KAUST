8dbfdb02861c357a555befac0afa4bc864804d04
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-10-24 09:34:09 +0200

    Update SUPPA.sh

--- SUPPA.sh
+++ SUPPA.sh
@@ -6,3 +6,6 @@ STAR --runMode genomeGenerate --runThreadN 8 --genomeSAindexNbases 13 --genomeFa
 
 ## STAR alignment ##
 STAR --runThreadN 8 --genomeDir ../Whole_genome/GenomeDir --readFilesIn M_19_0211_33-F-3-1_D705-D501_L001_R1_001.fastq M_19_0211_33-F-3-1_D705-D501_L001_R2_001.fastq --outSAMtype BAM Unsorted --quantMode TranscriptomeSAM --outFileNamePrefix STAR_BAM/M_19_0211
+
+# salmon quant #
+salmon quant -t ../../Whole_genome/Transcriptome.fa -l A -a M_19_0211Aligned.toTranscriptome.out.bam -o salmon_quant -p 8

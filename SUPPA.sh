4685bd54c2449ff319ecffa17e4f25e80555ff57
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-10-25 10:19:19 +0200

    Update SUPPA.sh

--- SUPPA.sh
+++ SUPPA.sh
@@ -9,7 +9,7 @@ STAR --runMode genomeGenerate --runThreadN 8 --genomeSAindexNbases 13 --genomeFa
 
 ## STAR alignment ##
 STAR --runThreadN 8 --genomeDir ../Whole_genome/GenomeDir --readFilesIn M_19_0211_33-F-3-1_D705-D501_L001_R1_001.fastq M_19_0211_33-F-3-1_D705-D501_L001_R2_001.fastq --outSAMtype BAM Unsorted --quantMode TranscriptomeSAM --outFileNamePrefix STAR_BAM/M_19_0211
-for R1 in *R1*; do R2=${R1//R1_001.fastq.gz/R2_001.fastq.gz}; sample=${R1%_R1_001.fastq.gz}; STAR --runThreadN 8 --genomeDir ../../Whole_genome/GenomeDir --readFilesIn <(gunzip -c $R1 -2 $R2) --outSAMtype BAM Unsorted --quantMode TranscriptomeSAM --outFileNamePrefix $sample; done
+for R1 in *R1*; do R2=${R1//R1_001.fastq.gz/R2_001.fastq.gz}; sample=${R1%_R1_001.fastq.gz}; STAR --runThreadN 8 --genomeDir ../Whole_genome/GenomeDir --readFilesIn <(gunzip -c $R1 $R2) --outSAMtype BAM Unsorted --quantMode TranscriptomeSAM --outFileNamePrefix STAR_align/$sample; done
 
 # salmon quant #
 salmon quant -t ../../Whole_genome/Transcriptome.fa -l A -a M_19_0211Aligned.toTranscriptome.out.bam -o salmon_quant -p 8

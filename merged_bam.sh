ff3e50157a4854b6c66d0cedb3f7cf192522d555
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2022-05-25 13:30:31 +0200

    Delete merged_bam.sh

--- merged_bam.sh
+++
@@ -1,8 +0,0 @@
-## merged ##
-stringtie --merge -p12 -G ~/Documents/DS/Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf *gtf -o merged.gtf
-
-## compare #
-gffcompare merged.gtf -r ~/Documents/DS/Whole_genome/Oryza_sativa.IRGSP-1.0.44.gtf -V
-
-# feature count ##
-featureCounts -T10 -a gffcmp.annotated.gtf -g transcript_id -o transcript_counts.txt ../*.bam  -G ~/Documents/DS/Whole_genome/whole_genome.fa
